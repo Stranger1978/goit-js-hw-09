4586c2f9c5d86c71ab4f9113b717f22b196300ea
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2022-11-30 01:21:23 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+
+const startBtn = document.querySelector('button[Data-Start]');
+const stopBtn = document.querySelector('button[Data-Stop]');
+let stopBtnDisabled = stopBtn.setAttribute("disabled", true);
+let startBtndisabled = startBtn.getAttribute("disabled", null);
+let timerId = null;
+
+startBtn.addEventListener('click', onStartClick);
+
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() { 
+    startBtndisabled = startBtn.setAttribute("disabled", true);
+    stopBtnDisabled = stopBtn.removeAttribute ("disabled");
+        timerId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);  
+    } 
+
+function onStopClick() { 
+    stopBtnDisabled = stopBtn.setAttribute("disabled", true);
+    startBtndisabled = startBtn.removeAttribute("disabled");
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
